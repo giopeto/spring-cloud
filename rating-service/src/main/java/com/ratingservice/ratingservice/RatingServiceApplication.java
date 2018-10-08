31bbfb9a3e3d00ffcf5275d97b945929a8e405dc
Georgy S. Georgiev <georgy.georgiev@musala.com>
2018-10-08 17:19:49 +0300

    first commit

---
+++ rating-service/src/main/java/com/ratingservice/ratingservice/RatingServiceApplication.java
@@ -0,0 +1,47 @@
+package com.ratingservice.ratingservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RequestMapping("/ratings")
+public class RatingServiceApplication {
+
+	@Value("${server.port}") private int port;
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingServiceApplication.class, args);
+	}
+
+	private List<Rating> ratingList = Arrays.asList(
+			new Rating(1L, 1L, 2, port),
+			new Rating(2L, 1L, 3, port),
+			new Rating(3L, 2L, 4, port),
+			new Rating(4L, 2L, 5, port)
+	);
+
+	@GetMapping("")
+	public List<Rating> findRatingsByBookId(@RequestParam Long bookId) {
+		return bookId == null || bookId.equals(0L) ?
+				Collections.EMPTY_LIST : ratingList.stream().filter(r -> r.getBookId().equals(bookId)).collect(Collectors.toList());
+	}
+
+	@GetMapping("/all")
+	public List<Rating> findAllRatings() {
+		ratingList.forEach(rating -> rating.setPort(port));
+		return ratingList;
+	}
+}
